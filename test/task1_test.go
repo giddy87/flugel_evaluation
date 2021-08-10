2cb94e6b9a684831ddd24a4e7eeced7661ba3b90
Gideon <gidahosa@claritae.com>
2021-08-10 18:35:32 +0100

    turn of terraform wrapper

--- test/task1_test.go
+++ test/task1_test.go
@@ -29,20 +29,10 @@ instanceID := terraform.Output(t, terraformOptions, "instance_id")
 
 instanceTags := aws.GetTagsForEc2Instance(t, awsRegion, instanceID)
 
-	// website::tag::3::Check if the EC2 instance with a given tag and name is set.
+
 	testingTag, containsFlugelNameTag := instanceTags["Name"]
 	assert.True(t, containsFlugelNameTag)
 	assert.Equal(t, "Flugel", testingTag)
-
-	// Verify that our expected name tag is one of the tags
-//	nameTag, containsNameTag := instanceTags["Name"]
-//	assert.True(t, containsNameTag)
-//	assert.Equal(t, expectedName, nameTag)instanceTags := aws.GetTagsForEc2Instance(t, awsRegion, instanceID)
-
-
-// tags := terraform.OutputMap(t, terraformOptions,"tags")
-// assert.Contains(t, tags, "Name: Flugel")
-// assert.Contains(t, tags, "Owner: InfraTeam")
 
 
 
